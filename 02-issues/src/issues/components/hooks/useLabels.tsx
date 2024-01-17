dbd361e9dedc0e831ddbbee2efc4b4fcc81b8ffe
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-01-17 18:22:33 -0400

    feat: add useLabels with useQuery

---
+++ 02-issues/src/issues/components/hooks/useLabels.tsx
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import { Label } from '../../../interfaces/labels';
+import { githubApi } from '../../../api/githubApi';
+import { sleep } from '../../../helpers/sleep';
+
+const getLabels = async (): Promise<Label[]> => {
+  await sleep(2);
+
+  const { data } = await githubApi.get<Label[]>('/labels');
+  return data;
+};
+
+export const useLabels = () => {
+  const labelsQuery = useQuery({
+    queryKey: ['labels'],
+    queryFn: getLabels,
+  });
+
+  return {
+    labelsQuery,
+  };
+};
