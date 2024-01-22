b92dc592e77a29c0f28c53bc5ea2ae59a6ac529c
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-01-22 16:05:14 -0400

    feat: modified props to added page by using button next and prev

--- 02-issues/src/issues/hooks/useIssues.tsx
+++ 02-issues/src/issues/hooks/useIssues.tsx
@@ -2,13 +2,19 @@ import { useQuery } from '@tanstack/react-query';
 import { githubApi } from '../../api/githubApi';
 import { Issue, State } from '../../interfaces';
 import { sleep } from '../../helpers';
+import { useEffect, useState } from 'react';
 
 interface Props {
   state?: State;
   labels: string[];
+  page?: number;
 }
 
-const getIssues = async (labels: string[], state?: State): Promise<Issue[]> => {
+const getIssues = async ({
+  labels,
+  state,
+  page = 1,
+}: Props): Promise<Issue[]> => {
   await sleep(2);
 
   const params = new URLSearchParams();
@@ -19,7 +25,7 @@ const getIssues = async (labels: string[], state?: State): Promise<Issue[]> => {
     params.append('labels', labelString);
   }
 
-  params.append('page', '1');
+  params.append('page', page.toString());
   params.append('per_page', '5');
 
   const { data } = await githubApi.get<Issue[]>('/issues', { params });
@@ -28,10 +34,35 @@ const getIssues = async (labels: string[], state?: State): Promise<Issue[]> => {
 };
 
 export const useIssues = ({ state, labels }: Props) => {
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setPage(1);
+  }, [state, labels]);
+
   const issuesQuery = useQuery({
-    queryKey: ['issues', { state, labels }],
-    queryFn: () => getIssues(labels, state),
+    queryKey: ['issues', { state, labels, page }],
+    queryFn: () => getIssues({ labels, state, page }),
   });
 
-  return { issuesQuery };
+  const nextPage = () => {
+    if (issuesQuery.data?.length === 0) return;
+    setPage(page + 1);
+  };
+
+  const prevPage = () => {
+    if (page > 1) setPage(page - 1);
+  };
+
+  return {
+    // Properties
+    issuesQuery,
+
+    // Getter
+    page: issuesQuery.isFetching ? 'Loading...' : page,
+
+    // Methods
+    nextPage,
+    prevPage,
+  };
 };
