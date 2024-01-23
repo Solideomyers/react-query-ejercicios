0fbbf7847fc6f4e11c5a8bb86f41646ad38d26a7
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-01-23 19:57:30 -0400

    feat: add action by apis

---
+++ 03-react-query-store/src/products/services/actions.ts
@@ -0,0 +1,19 @@
+import { type Product, productsApi } from '..';
+
+interface GetProductsOptions {
+  filterKey?: string;
+}
+
+export const getProducts = async ({
+  filterKey,
+}: GetProductsOptions): Promise<Product[]> => {
+  const filterUrl = filterKey ? `category=${filterKey}` : '';
+
+  const { data } = await productsApi.get<Product[]>(`/products?${filterUrl}`);
+  return data;
+};
+
+export const getProductById = async (id: string): Promise<Product> => {
+  const { data } = await productsApi.get<Product>(`/products/${id}`);
+  return data;
+};
