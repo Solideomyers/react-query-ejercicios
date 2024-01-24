531feab69920a5d3967dabb47fae997d5469d13e
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-01-23 20:49:49 -0400

    feat: refactor

--- 03-react-query-store/src/products/pages/CompleteListPage.tsx
+++ 03-react-query-store/src/products/pages/CompleteListPage.tsx
@@ -1,13 +1,14 @@
-import { ProductList } from ".."
-
+import { ProductList, useProducts } from '..';
 
 export const CompleteListPage = () => {
+  const { isLoading, products } = useProducts({});
+
   return (
-    <div className="flex-col">
-      <h1 className="text-2xl font-bold">Todos los productos</h1>
+    <div className='flex-col'>
+      <h1 className='text-2xl font-bold'>Todos los productos</h1>
+      {isLoading && <p>Cargando...</p>}
 
-      <ProductList />
-
+      <ProductList products={products} />
     </div>
-  )
-}
+  );
+};
