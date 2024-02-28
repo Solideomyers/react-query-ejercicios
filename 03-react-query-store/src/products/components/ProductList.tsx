24f5c833ae5144de3d9a9eab9009646daa74cfd0
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-02-28 19:27:23 -0400

    fix: refactor styles in product list

--- 03-react-query-store/src/products/components/ProductList.tsx
+++ 03-react-query-store/src/products/components/ProductList.tsx
@@ -8,7 +8,7 @@ export const ProductList = ({ products }: Props) => {
   const { prefetchProduct } = usePrefetchProduct();
 
   return (
-    <div className='mt-2 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-2 justify-center max-w-max'>
+    <div className='mt-2 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-2 max-w-max'>
       {products.map((product) => (
         <ProductCard
           key={product.id}
